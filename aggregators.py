46affe6858d795c1f219b5f3abe8ed706b282d6c
isspek <ipekbrs@gmail.com>
2020-01-23 15:32:38 +0100

    added initial of evaluation

--- aggregators.py
+++ aggregators.py
@@ -3,9 +3,11 @@
 '''
 module name: e.g misinfome
 result: cred, confidence values, label
-
 get all values and take average, then set it to final label
 '''
+
+
+
 def default(credibility_results):
     count = 0
     for module_name, result in credibility_results.items():
@@ -17,3 +19,10 @@ def check_single_module(single_module_result):
     pass
 
 # todo add probabilistic evaluation methods
+
+
+aggregators: {
+
+    'default' : default
+
+}
--- evaluation.py
+++ evaluation.py
@@ -1,8 +1,59 @@
-## post request
-# http request gataway in order retrieve outputs
+import os
+from pathlib import Path
+
+import pandas as pd
+
+from aggregators import *
+
+'''
+Pipeline for the evaluation
+
+'''
+
+DATA_DIR = Path(os.path.dirname(os.path.dirname(__file__))) / Path(os.path.basename(os.path.dirname(__file__))) / 'data'
+
+
+def callback_aggregate(row, mode = 'default'):
+    dummy_request = _create_request(row)
+    response = aggregators[mode](dummy_request)
+    return response
+
+
+def _create_request(row):
+    return {
+
+        'misinfome': {
+            'cred': row['misinfome_creds'],
+            'conf': row['misinfome_confs']
+        },
+        'stance': {
+            'cred': row['content_analys_creds'],
+            'conf': row['content_analys_conf']
+        },
+        'claim_credibility': {
+            'cred': row['claim_creds'],
+            'conf': row['claim_conf']
+        }
+
+    }
+
+
+def run():
+    for file_name in DATA_DIR.glob('*.csv'):
+        dummy_values = pd.read_csv(file_name, low_memory=False)
+        ground_labels = dummy_values['expected_credible']
+        dummy_values['actual_credible'] = dummy_values.apply(lambda row: callback_aggregate)
+
+        # glued_data = pd.concat([glued_data, x], axis=0)
+
+    pass
+
 
 ## aggregation
 
 
-
 # check sepearetly
+
+
+if __name__ == '__main__':
+    run()
