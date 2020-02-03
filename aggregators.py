6788846d1405dd68c7b88d6ea49031adab5de85a
Akram Sadat Hosseini <45170827+AkramSadat@users.noreply.github.com>
2020-02-03 16:14:13 +0100

    Update aggregators.py

--- aggregators.py
+++ aggregators.py
@@ -7,10 +7,7 @@ import csv
 
 import numpy as np
 import pandas as pd
-import heapq
 
-# this is default method
-from scipy.sparse import data
 
 '''
 module name: e.g misinfome
@@ -18,18 +15,7 @@ result: cred, confidence values, label
 get all values and take average, then set it to final label
 '''
 
-DATA_DIR = Path(os.path.dirname(os.path.dirname(__file__))) / Path(os.path.basename(os.path.dirname(__file__))) / 'data'
-for file_name in DATA_DIR.glob('*.csv'):
-    results = {}
-    name = file_name.name[:-4]
-    results['collection'] = name
-    dummy_values = pd.read_csv(file_name)
-    claim_conf = dummy_values['claim_conf'].astype(float).values.tolist()
-    claim_cred = dummy_values['claim_cred'].astype(float).values.tolist()
-    misinfome_conf = dummy_values['misinfome_conf'].astype(float).values.tolist()
-    misinfome_cred = dummy_values['misinfome_cred'].astype(float).values.tolist()
-    content_analys_conf = dummy_values['content_analys_conf'].astype(float).values.tolist()
-    content_analys_cred = dummy_values['content_analys_cred'].astype(float).values.tolist()
+
 
 
 
@@ -41,54 +27,64 @@ THRESHOLDS = {
     'not_credible': -0.99,
 }
 
+outputs = []
+thresholds = sorted(THRESHOLDS.items(), key=lambda t: t[1], reverse=True)
+
 CRED_WEIGHTS = [['misinfome',1], ['content_analys',4], ['claim',3]]
 
 
 
 def maximum(credibility_results):
+    misinfome_conf = credibility_results["misinfome"]["conf"]
+    content_analys_conf = credibility_results["stance"]["conf"]
+    claim_conf = credibility_results["claim_credibility"]["conf"]
+
+    misinfome_cred = credibility_results["misinfome"]["cred"]
+    content_analys_cred = credibility_results["stance"]["cred"]
+    claim_cred = credibility_results["claim_credibility"]["cred"]
+
+
 
     maximum_confidence = np.maximum(np.maximum(misinfome_conf, content_analys_conf), claim_conf)
-
     maximum_credibility = np.maximum(np.maximum(misinfome_cred, claim_cred), content_analys_cred)
 
-
-
-    outputs = []
     thresholds = sorted(THRESHOLDS.items(), key=lambda t: t[1], reverse=True)
 
-    for conf in maximum_confidence:
-        if conf < 0.5:
-            outputs.append('not_verifiable')
-        else:
-            for cred in maximum_credibility:
-                for label, threshold in thresholds:
-                    if cred <= threshold:
-                        outputs.append(label)
-                        print(cred)
-                        break
+    if maximum_confidence < 0.5:
+        outputs.append('not_verifiable')
+    elif maximum_confidence >= 0.5:
+        for label, threshold in thresholds:
+            if maximum_credibility <= threshold:
+                outputs.append(label)
+                break
     for item in outputs:
         return item
 
 
 
-def default(credibility_results):
-        count =0
-        outputs = []
-        thresholds = sorted(THRESHOLDS.items(), key=lambda t: t[1], reverse=True)
 
-        for label, threshold in thresholds:
-            for cred1 in misinfome_cred:
-                for cred2 in claim_cred:
-                    if (cred1 > threshold) & (cred2 > threshold):
-                        outputs.append(label)
-                for cred3 in content_analys_cred:
-                    if (cred1 > threshold) & (cred3 > threshold):
-                        outputs.append(label)
-                if (cred2 > threshold) & (cred3 > threshold):
+
+
+def default(credibility_results):
+
+    misinfome_cred = credibility_results["misinfome"]["cred"]
+    content_analys_cred = credibility_results["stance"]["cred"]
+    claim_cred = credibility_results["claim_credibility"]["cred"]
+
+
+
+    for label, threshold in thresholds:
+                if (misinfome_cred > threshold) & (content_analys_cred > threshold):
                     outputs.append(label)
+                elif (misinfome_cred > threshold) & (claim_cred > threshold):
+                    outputs.append(label)
+                elif (content_analys_cred > threshold) & (claim_cred > threshold):
+                    outputs.append(label)
+                else:
+                    outputs.append('not_verifiable')
                     break
-        for item in outputs:
-            return item
+    for item in outputs:
+        return item
 
 
 def dummy_output(credibility_results):
@@ -100,30 +96,37 @@ def dummy_output(credibility_results):
 
 
 def weighted_average(credibility_results):
-        weighted_average_credibility = (dummy_values.misinfome_cred * CRED_WEIGHTS[0][1] +
-                                        dummy_values.claim_cred * CRED_WEIGHTS[1][1] +
-                                        dummy_values.content_analys_cred * CRED_WEIGHTS[2][1]) / (
+    misinfome_conf = credibility_results["misinfome"]["conf"]
+    content_analys_conf = credibility_results["stance"]["conf"]
+    claim_conf = credibility_results["claim_credibility"]["conf"]
+
+    misinfome_cred = credibility_results["misinfome"]["cred"]
+    content_analys_cred = credibility_results["stance"]["cred"]
+    claim_cred = credibility_results["claim_credibility"]["cred"]
+
+    weighted_average_credibility = (misinfome_cred * CRED_WEIGHTS[0][1] +
+                                        claim_cred * CRED_WEIGHTS[1][1] +
+                                        content_analys_cred * CRED_WEIGHTS[2][1]) / (
                                                    CRED_WEIGHTS[0][1] + CRED_WEIGHTS[1][1] + CRED_WEIGHTS[2][1])
 
-        weighted_average_confidence = (dummy_values.misinfome_conf * CRED_WEIGHTS[0][1] +
-                                        dummy_values.claim_conf * CRED_WEIGHTS[1][1] +
-                                        dummy_values.content_analys_conf * CRED_WEIGHTS[2][1]) / (
+    weighted_average_confidence = (misinfome_conf * CRED_WEIGHTS[0][1] +
+                                        claim_conf * CRED_WEIGHTS[1][1] +
+                                        content_analys_conf * CRED_WEIGHTS[2][1]) / (
                                                    CRED_WEIGHTS[0][1] + CRED_WEIGHTS[1][1] + CRED_WEIGHTS[2][1])
 
-        outputs = []
-        thresholds = sorted(THRESHOLDS.items(), key=lambda t: t[1], reverse=True)
 
-        for cred in weighted_average_credibility:
-            for conf in weighted_average_confidence:
-                if conf < 0.5:
-                    outputs.append('not_verifiable')
-                elif conf >= 0.5:
-                    for label, threshold in thresholds:
-                        if cred >= threshold:
-                            outputs.append(label)
-                            break
-        for item in outputs:
-            return item
+
+    if weighted_average_confidence < 0.5:
+        outputs.append('not_verifiable')
+    else:
+        for label, threshold in thresholds:
+            if weighted_average_credibility <= threshold:
+                outputs.append(label)
+                break
+    for item in outputs:
+        return item
+
+
 
 
 methods = {
